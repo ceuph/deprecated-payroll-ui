2a94a54ffdb7183d12d6d794336933141f620d3e
Joey O. Chua <joeyochua@gmail.com>
2019-10-15 16:38:55 +0800

    remove duplicate in retrieval of last period

--- _protected/views/pay-period/_form.php
+++ _protected/views/pay-period/_form.php
@@ -7,6 +7,7 @@ use kartik\date\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model app\models\PayPeriod */
 /* @var $form yii\widgets\ActiveForm */
+/* @var $last app\models\PayPeriod */
 ?>
 
 <div class="pay-period-form">
