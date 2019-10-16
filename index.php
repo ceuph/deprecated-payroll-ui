212414fe8f9ad5f6ab8a669d0f5d029ff2fcf958
Joey O. Chua <joeyochua@gmail.com>
2019-10-16 11:04:36 +0800

    add dynamic index add docker compose

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require(__DIR__ . '/_protected/vendor/autoload.php');
-require(__DIR__ . '/_protected/vendor/yiisoft/yii2/Yii.php');
-
-$config = require(__DIR__ . '/_protected/config/web.php');
+if (file_exists(__DIR__ . '/../_protected/')) {
+    defined('YII_PROTECTED_DIR') or define('YII_PROTECTED_DIR', __DIR__ . '/../_protected');
+} else {
+    defined('YII_PROTECTED_DIR') or define('YII_PROTECTED_DIR', __DIR__ . '/_protected');
+}
+require(YII_PROTECTED_DIR . '/vendor/autoload.php');
+require(YII_PROTECTED_DIR . '/vendor/yiisoft/yii2/Yii.php');
+$config = require(YII_PROTECTED_DIR .  '/config/web.php');
 
 (new yii\web\Application($config))->run();
